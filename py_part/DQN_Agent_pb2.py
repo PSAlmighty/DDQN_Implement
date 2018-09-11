bbc363b369c2e17715fee38e04147a2aed074c46
Fox <nena2749@gmail.com>
2018-09-11 10:55:29 +0800

    17th

--- py_part/DQN_Agent_pb2.py
+++ py_part/DQN_Agent_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   name='DQN_Agent.proto',
   package='',
   syntax='proto3',
-  serialized_pb=_b('\n\x0f\x44QN_Agent.proto\"\xa5\x01\n\nAgent_Info\x12\x14\n\x0c\x41gent_Action\x18\x01 \x01(\x07\x12\x18\n\x10\x43urrent_Position\x18\x02 \x01(\x07\x12 \n\x18\x41gent_Trading_Instrument\x18\x03 \x01(\t\x12\x18\n\x10\x41\x63tion_Timestamp\x18\x04 \x01(\t\x12\x14\n\x0cLatestResult\x18\x05 \x01(\x08\x12\x15\n\rmsg_pipe_init\x18\x06 \x01(\x08\"q\n\x07MD_Info\x12\x12\n\nlast_price\x18\x01 \x01(\x01\x12\x0e\n\x06volume\x18\x02 \x01(\x07\x12\x15\n\rmsg_pipe_init\x18\x03 \x01(\x08\x12\x14\n\x0cMD_Timestamp\x18\x04 \x01(\t\x12\x15\n\rMD_Instrument\x18\x05 \x01(\tb\x06proto3')
+  serialized_pb=_b('\n\x0f\x44QN_Agent.proto\"\xa5\x01\n\nAgent_Info\x12\x14\n\x0c\x41gent_Action\x18\x01 \x01(\x07\x12\x18\n\x10\x43urrent_Position\x18\x02 \x01(\x07\x12 \n\x18\x41gent_Trading_Instrument\x18\x03 \x01(\t\x12\x18\n\x10\x41\x63tion_Timestamp\x18\x04 \x01(\t\x12\x14\n\x0cLatestResult\x18\x05 \x01(\x08\x12\x15\n\rmsg_pipe_init\x18\x06 \x01(\x08\"\x88\x01\n\x07MD_Info\x12\x12\n\nlast_price\x18\x01 \x01(\x01\x12\x0e\n\x06volume\x18\x02 \x01(\x07\x12\x15\n\rmsg_pipe_init\x18\x03 \x01(\x08\x12\x14\n\x0cMD_Timestamp\x18\x04 \x01(\t\x12\x15\n\rMD_Instrument\x18\x05 \x01(\t\x12\x15\n\rMD_TradingDay\x18\x06 \x01(\tb\x06proto3')
 )
 
 
@@ -133,6 +133,13 @@ _MD_INFO = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='MD_TradingDay', full_name='MD_Info.MD_TradingDay', index=5,
+      number=6, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -145,8 +152,8 @@ _MD_INFO = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=187,
-  serialized_end=300,
+  serialized_start=188,
+  serialized_end=324,
 )
 
 DESCRIPTOR.message_types_by_name['Agent_Info'] = _AGENT_INFO
